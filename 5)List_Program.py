7cd66f07f878748e8713a7cfdaefb231c378dd47
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-11-08 22:16:35 +0530

    List datastructure program is added

---
+++ 5)List_Program.py
@@ -0,0 +1,31 @@
+#List is the data structure in which we can store multiple value 
+
+#we can stoire different types of values in it .
+
+list_of_cloud = ["aws","azure","gcp","oracle","digital","ocean","utho","alibaba"]
+
+print(list_of_cloud)
+
+# add new value at the end of the string
+
+list_of_cloud.append("Salesforce")
+
+print(list_of_cloud)
+
+list_of_cloud.append("IBM")
+
+print(list_of_cloud)
+
+# add the new value at the index of the list 
+
+list_of_cloud.insert(2,"Accenture")
+
+print(list_of_cloud)
+
+# tell the length of the list
+
+print(len(list_of_cloud))
+
+
+
+
