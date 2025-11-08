3d62740341bd21adac318974fea73348f88936e2
Nikita Bhoyar <nikitabhoyar99@gmail.com>
2025-11-08 22:26:37 +0530

    For loop program is coded

---
+++ 6)For_Loop_Program.py
@@ -0,0 +1,10 @@
+list_of_cloud = ["aws", "azure","gcp","oracle","IBM","ZS", "Accenture","Infosys"]
+
+print(list_of_cloud)
+
+for cloud in list_of_cloud:
+    print(cloud)
+
+for i in range(1,10):
+    print(i)
+
